1d3f9e31bbf37c95154fd3c9f5fd585329a2fb3d
minh.hao <minhhao767@gmail.com>
2023-08-14 16:38:19 +0700

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,20 @@
+// const mongoose = require("mongoose");
+
+// function connect() {
+//   mongoose
+//     .connect("mongodb://localhost:27017/admin")
+//     .then(() => console.log("Connected to db!"))
+//     .catch((err) => console.log("Failed to connect"));
+// }
+
+// module.exports = { connect };
+
 const mongoose = require("mongoose");
+const uri = "mongodb+srv://minhhao767:KoOe3lFnNZIXGBbP@cluster0.m73rtd1.mongodb.net/?retryWrites=true&w=majority";
 
 function connect() {
   mongoose
-    .connect("mongodb://localhost:27017/admin")
+    .connect(uri, { useNewUrlParser: true })
     .then(() => console.log("Connected to db!"))
     .catch((err) => console.log("Failed to connect"));
 }
